da56b4fc0f51f26fc0a7a62e57db8ade83ab2a11
Mooseok Bahng <msbahng@gmail.com>
2024-02-05 21:59:46 +0900

    added Logger

--- Package.swift
+++ Package.swift
@@ -18,15 +18,20 @@ let package = Package(
             targets: ["CodeScanner"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            url:  "git@github.com:msbahng/LoggeriOS.git",
+            .upToNextMajor(from: "1.0.0")
+//            path: "../LoggeriOS"
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CommonUtils",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Logger", package: "LoggeriOS")
+            ]),
         .target(
             name: "CodeScanner",
             dependencies: []),
