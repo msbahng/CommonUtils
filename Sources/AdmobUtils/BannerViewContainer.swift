aaf60bd56d70f91d60e869ae80fc1636b192f3cc
Mooseok Bahng <msbahng@gmail.com>
2025-05-01 20:30:24 +0900

    modified ad banner view

---
+++ Sources/AdmobUtils/BannerViewContainer.swift
@@ -0,0 +1,74 @@
+//
+//  BannerViewContainer.swift
+//  gourmet
+//
+//  Created by 方 茂碩（Mooseok Bahng） on 2024/05/31.
+//
+
+import SwiftUI
+import Logger
+import GoogleMobileAds
+
+public struct BannerViewContainer: UIViewRepresentable {
+    
+    public static let defaultBannerSize = AdSizeBanner
+    
+    private let adUnitID: String
+    private let bannerSize: AdSize
+    @Binding private var isAdLoaded: Bool
+    
+    private let bannerView = AdManagerBannerView()
+    
+    public init(
+        adUnitID: String,
+        bannerSize: AdSize = Self.defaultBannerSize,
+        isAdLoaded: Binding<Bool>
+    ) {
+        self.adUnitID = adUnitID
+        self.bannerSize = bannerSize
+        _isAdLoaded = isAdLoaded
+    }
+    
+    public func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+        view.addSubview(context.coordinator.bannerView)
+        return view
+    }
+    
+    public func updateUIView(_ uiView: UIView, context: Context) {
+        context.coordinator.bannerView.adSize = bannerSize
+        context.coordinator.bannerView.center = uiView.center
+    }
+    
+    public func makeCoordinator() -> BannerCoordinator {
+        return BannerCoordinator(self)
+    }
+    
+    @MainActor
+    public class BannerCoordinator: NSObject, BannerViewDelegate {
+        
+        private(set) lazy var bannerView: BannerView = {
+            let banner = BannerView(adSize: parent.bannerSize)
+            banner.adUnitID = parent.adUnitID
+            banner.load(Request())
+            banner.delegate = self
+            return banner
+        }()
+        
+        let parent: BannerViewContainer
+        
+        init(_ parent: BannerViewContainer) {
+            self.parent = parent
+        }
+        
+        public func bannerViewDidReceiveAd(_ bannerView: BannerView) {
+            Logger.printLog("\(#function) called")
+            parent.isAdLoaded = true
+        }
+        
+        public func bannerView(_ bannerView: BannerView, didFailToReceiveAdWithError error: Error) {
+            Logger.printLog("\(#function) called")
+            parent.isAdLoaded = false
+        }
+    }
+}
